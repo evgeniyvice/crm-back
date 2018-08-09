e940e871ab51cee19de29d4906893cc7d2add641
Евгений Вайс <e.vice@cti.ru>
2018-08-09 13:21:08 +0300

    сделал добавление пользователя при регистрации

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
+const bcrypt = require('bcryptjs')
 const User = require('../models/User')
 
-module.exports.login = function(req, res) {
+module.exports.login = function (req, res) {
     res.status(200).json({
         login: {
             email: req.body.email,
@@ -9,13 +10,29 @@ module.exports.login = function(req, res) {
     })
 }
 
-module.exports.registration = async function(req, res) {
-    
-    const candidate = await User.findOne({email: req.body.email})
+module.exports.registration = async function (req, res) {
 
-    if(candidate) {
+    const candidate = await User.findOne({
+        email: req.body.email
+    })
+
+    if (candidate) {
         res.status(409).json({
             message: 'Пользователь с таким email уже существует'
         })
+    } else {
+        const salt = bcrypt.genSaltSync(10)
+        const password = req.body.password
+        const user = new User({
+            email: req.body.email,
+            password: bcrypt.hashSync(password, salt)
+        })
+
+        try {
+            await user.save()
+            res.status(201).json(user)
+        } catch (e) {
+
+        }
     }
 }
